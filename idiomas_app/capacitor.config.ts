9a23a7971187bd8d925628933551ea5dcffaa9e1
Leon Sokolowski <leonsokolowski2104@gmail.com>
2025-04-25 22:30:52 -0300

    a

---
+++ idiomas_app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'idiomas_app',
+  webDir: 'www'
+};
+
+export default config;
---
+++ login_app/android/app/src/main/java/com/loginapp/leon/MainActivity.java
@@ -0,0 +1,5 @@
+package com.loginapp.leon;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
