831cb12aea87811fa7158f4d7bcd8646b7c97c5f
Leon Sokolowski <leonsokolowski2104@gmail.com>
2025-05-02 01:47:57 -0300

    a

--- idiomas_app/src/app/pages/home/home.component.ts
+++ idiomas_app/src/app/pages/home/home.component.ts
@@ -10,17 +10,31 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule,IonContent,IonButton,IonCard, IonCardContent, IonCardHeader, IonCardTitle, RouterModule]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule,IonContent, RouterModule]
 })
 export class HomeComponent implements OnInit {
   auth = inject(AuthService);
+  idioma: string = 'es';
+  tema: string = 'colores';
+
   constructor() { }
 
   logout()
   {
     this.auth.cerrarSesion();
   }
+
   ngOnInit() {
+  }
+
+  cambiarIdioma(idioma: string) {
+    this.idioma = idioma;
+    console.log(`Idioma cambiado a: ${idioma}`);
+  }
+
+  cambiarTema(tema: string) {
+    this.tema = tema;
+    console.log(`Tema cambiado a: ${tema}`);
   }
 
 }
