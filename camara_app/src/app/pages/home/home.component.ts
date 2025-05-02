2dbf9740bf7c0346032774a220391fac9947c441
leonsokolowski <leonsokolowski2104@gmail.com>
2025-05-01 23:02:09 -0300

    a

--- camara_app/src/app/pages/home/home.component.ts
+++ camara_app/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FormBuilder, FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule} from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle} from '@ionic/angular/standalone';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+// import { IonIcon } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
